946f7681f5a4f3e7ecbea61bed6dbf0cfe0f2eb9
hans4858 <hans4858@morris.umn.edu>
2015-12-11 21:24:20 -0600

    we done

--- src/huffmanEncoding.java
+++ src/huffmanEncoding.java
@@ -12,11 +12,11 @@ public class huffmanEncoding {
 		String input = new String();
 		
 		input = scanner.nextLine();
-		System.out.println(input);
+		//System.out.println(input);
 		String[] tokens = input.split(" ");
 		
 		for(int i = 0; i < tokens.length; i += 2){
-			System.out.println(i);
+			//System.out.println(i);
 			nodeArray.add(new freqNode(tokens[i].charAt(0), Integer.parseInt(tokens[i+1])));
 		}
 		
@@ -29,28 +29,36 @@ public class huffmanEncoding {
 			left = nodeArray.get(length - 1);
 			right = nodeArray.get(length - 2);
 			placeholder = new freqNode(left.freq + right.freq, left, right);
+			
+			//System.out.println(nodeArray);
+			
 			nodeArray.remove(length - 1);
 			nodeArray.remove(length - 2);
-			for(int i = length - 2; i > 0; i--){
+			int i = 0;
+			for(i = nodeArray.size()-1; i >= 0; i--){
 				if(nodeArray.get(i).freq > placeholder.freq){
-					nodeArray.add(i + 1, placeholder);
+					//System.out.println(i);
+					//i = i;
 					break;
 				}
 			}
-			
+			nodeArray.add(i+1, placeholder);
 			length = nodeArray.size();
-		
+			//System.out.println(nodeArray.get(0).freq);
 		}
+		
 		encoder("",nodeArray.get(0));
-		//TODO: put encodings here.
+		
 		char command;
+		
 		boolean continueReading = true;
+		
 		while(continueReading){
-			command = (char) scanner.nextInt();
+			command = scanner.next().charAt(0);
 			if(command == 'e'){
-				System.out.println(encode(codeArray, scanner.nextLine()));
+				System.out.println("Encode: " + encode(nodeArray.get(0), scanner.next()));
 			}else if(command == 'd'){
-				System.out.println(decode(nodeArray, scanner.nextLine()));
+				System.out.println("Decode: " + decode(nodeArray.get(0), scanner.next()));
 			}else if(command == 'x'){
 				continueReading = false;
 			}
@@ -78,41 +86,86 @@ public class huffmanEncoding {
 		}
 	}
 	
-	public static String encode(ArrayList<freqNode> codeArray, String plainText){
+	public static String encode(freqNode node, String plainText){
 		String returnString = "";
-			char[] charArray = plainText.toCharArray();
-			
-			for(int i = 0; i < charArray.length; i++){
-				for(int j = 0; j < codeArray.size(); j++){
-					if(codeArray.get(j).letter == charArray[i]){
-						returnString.concat(codeArray.get(j).code);
-						j = codeArray.size();
-					}
-				}
-			}
+		char[] charArray = plainText.toCharArray();
+		String encodeString = "";
+		//System.out.println(charArray.length);
+		char c;
+		for(int i = 0; i < charArray.length; i++){
+			c = charArray[i];
+			//System.out.println(c);
+			encodeString = getEncode(node,c);
+			//System.out.println("endcodeded " + encodeString);
+			returnString += encodeString;
+			//System.out.println("returnString: " + returnString);
+		}
+		
+		//System.out.println(returnString);	
 		return returnString;
 	}
 	
-	public static String decode(ArrayList<freqNode> nodeArray, String codedText){
-		String returnString = "";
-		freqNode topNode = nodeArray.get(0);
-		freqNode currentNode = topNode;
-		char[] charArray = codedText.toCharArray();
-		for(int i = 0; i < charArray.length; i++){
-			if(currentNode.left != null && currentNode.right != null){
-				if(charArray[i] == '0'){
-					currentNode = currentNode.left;
-				}else if(charArray[i] == '1'){
-					currentNode = currentNode.right;
+	public static String getEncode(freqNode node, char target){
+		String encoding = "";
+		String leftResult;
+		String rightResult;
+			if(node.letter==target){
+				encoding = node.code;
+				//System.out.println("current node = " + node.letter);
+			}else {
+				if(node.left != null){
+					//System.out.println("leftR");
+					leftResult = getEncode(node.left, target);
+					if(leftResult.compareTo("") != 0){
+						encoding = leftResult;
+					}
 				}
-			}else{
-				returnString.concat(Character.toString(currentNode.letter));
+				if(node.right != null){
+					//System.out.println("rightR");
+					rightResult = getEncode(node.right, target);
+					if(rightResult.compareTo("") != 0){
+						encoding = rightResult;
+					}
+				}	
+			}
+		//System.out.println("code " + encoding);
+		return encoding;
+	}
+	
+	public static String decode(freqNode node, String codedText){
+		//String returnString = "";
+		freqNode topNode = node;
+		char[] charArray = codedText.toCharArray();
+		
+		//System.out.println("return " + returnString);
+		return getDecode(topNode, charArray);
+	}
+	
+	public static String getDecode(freqNode node, char[] charArray){
+		String decoding = "";
+		
+		freqNode currentNode = node;
+		
+		for(int i = 0; i < charArray.length; i++){
+			//System.out.println("code: " + charArray[i]);
+			if(charArray[i] == '0' && currentNode.left != null){
+				//leftResult
+				//System.out.println("left");
+				currentNode = currentNode.left;
+			} else if(charArray[i] == '1' && currentNode.right != null){
+				//rightResult
+				//System.out.println("right");
+				currentNode = currentNode.right;
+			}
+			if(currentNode.left == null && currentNode.right == null){
+				decoding += currentNode.letter;
+				currentNode = node;
 			}
 		}
-		
-		return returnString;
+		//System.out.println("decoding " + decoding);
+		return decoding;
 	}
-
+	
 }
 
 
